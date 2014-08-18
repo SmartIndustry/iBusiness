2db933aff57bc9b0d30e4597c8686520c089e02a
Roooooys <maming_jun@hotmail.com>
2014-08-18 10:30:42 +0800

    git 提交测试。代码样式修正

--- src/txActiviti/src/com/mossle/addresslist/web/AddressListController.java
+++ src/txActiviti/src/com/mossle/addresslist/web/AddressListController.java
@@ -19,23 +19,22 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 @RequestMapping("addresslist")
 public class AddressListController {
+    
     private JdbcTemplate jdbcTemplate;
 
     @RequestMapping("address-list-list")
-    public String list(
-            @RequestParam(value = "username", required = false) String username,
-            Model model) {
-        String sql = "select ub.username as username,ub.display_name as displayName,ub.email as email,ub.mobile as mobile"
-                + " from user_base ub" + " where ub.username like ?";
+    public String list( @RequestParam(value = "username", required = false) String username, Model model) {
+        String sql = " select ub.username as username,ub.display_name as displayName,ub.email as email,ub.mobile as mobile" + 
+                     " from user_base ub where ub.username like ?";
         List list = jdbcTemplate.queryForList(sql, "%" + username + "%");
         model.addAttribute("list", list);
 
         return "addresslist/address-list-list";
     }
 
-    // ~ ======================================================================
     @Resource
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        
         this.jdbcTemplate = jdbcTemplate;
     }
 }
